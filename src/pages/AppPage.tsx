bf074187c632664fd2b92d8ed23c1b325134b228
Adam Krivka <akrivka@caltech.edu>
2022-08-09 13:39:52 +0200

    added syncing label to the top right

--- src/pages/AppPage.tsx
+++ src/pages/AppPage.tsx
@@ -1,15 +1,23 @@
 import { Outlet } from "solid-app-router";
-import { Component } from "solid-js";
+import { Component, Show } from "solid-js";
+import { useEntries } from "../context/EntriesContext";
 
 const Page: Component = () => {
+  const { syncState } = useEntries();
   return (
     <>
-      {/* <div class="flex w-full justify-end">
-          <NavLink href="/">Go back</NavLink>
-        </div> */}
+      <Show
+        when={
+          syncState.remote.pushingUpdates() || syncState.remote.pullingUpdates()
+        }
+      >
+        <div class="absolute top-1 right-1 text-sm text-gray-400">
+          Syncing...
+        </div>
+      </Show>
       <Outlet />
     </>
   );
 };
 
-export default Page
+export default Page;
