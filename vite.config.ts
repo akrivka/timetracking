f0c91b6088e1fb26d4a06ad8d8f67970bbab7550
Adam Krivka <akrivka@caltech.edu>
2022-08-31 10:23:53 +0100

    second try?

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
       registerType: "autoUpdate",
       workbox: {
         globPatterns: ["**/*.{js,css}"],
+        navigateFallback: null,
       },
       // mode: "development",
       // devOptions: {
